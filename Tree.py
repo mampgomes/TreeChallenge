88192a5d4e9d11bd0a8a87295e83207ae6e005dd
mampgomes <mampgomes@gmail.com>
2021-05-13 17:57:12 +0100

    Final version of all files.

---
+++ Tree.py
@@ -0,0 +1,98 @@
+class Tree:
+    '''criacao de node
+    @param self node actual
+    @param val id da categoria a adicionar
+    @param nome nome da categoria a adicionar'''
+    def __init__(self, val, nome):
+        self.val = val
+        self.nome = nome
+        self.nodes = []
+
+    '''procura node pai (root caso nao especificado) e cria node filho
+    caso node pai nao exista adiciona a root
+    @param self node actual
+    @param val id da categoria a adicionar
+    @param nome nome da categoria a adicionar
+    @node_ID id da categoria pai'''
+    def add_node(self, val, nome, dad = "root"):
+        if(self.find_node(dad) == None):
+            return None
+        if(self.find_node(nome))!= None:
+            return None
+        self.find_node(dad).nodes.append(Tree(val, nome))
+        return self
+
+    '''compara valor dado com o dos nodes filhos ate encontrar o node a remover
+    testa os valores dos nodes filhos
+    desce de profundidade quando nao encontra na actual
+    @param self node actual   @param val id da categoria a adicionar
+    @return node pai caso removido ou root caso contrario'''
+    def remove_node(self, name):
+        queue = [self]
+        
+        for cat in queue:
+            for testing in cat.nodes:
+                if name == testing.nome:
+                    cat.nodes.remove(testing)
+                    return cat
+            queue.extend(cat.nodes)
+        return None
+    
+    '''procura node dando prioridade aos nodes do mesmo nivel (aka leitura horizontal)
+    procura nos filhos dos nodes e caso nao encontre
+    adiciona nodes do proximo nivel a queue
+    @param self node actual
+    @param val id da categoria a adicionar    
+    @return node caso encontrado ou root caso contrario'''        
+    def find_node(self, nome):
+        queue = [self]
+
+        for cat in queue:
+            if cat.nome == nome:
+                return cat
+            else:
+                queue.extend(cat.nodes)
+        return None
+    
+    def printTable(self, nome = ''):
+        flag = False
+        if nome == '':
+            flag = True
+            nome ="root"
+        table = self.find_node(nome)
+        if(len(table.nodes) > 0):
+            print("%s(%s):\n" %(table.nome, table.val))
+            for record in table.nodes:
+                print("\t%s" % (record.nome))
+            if flag:
+                for record in table.nodes:
+                    self.printTable(record.nome)
+
+    '''representacao do node em string
+    @param self node actual
+    @return string formatada'''
+    def __repr__(self):
+        return "(%s)%s\n\t%s" % (self.val, self.nome, self.nodes)
+
+if __name__ == "__main__":
+    a = Tree(0, "root")
+    a.add_node(1, "cat1")
+    a.add_node(1, "cat1")
+    a.add_node(2, "cat2")
+    a.add_node(3, "cat3")
+    a.add_node(4, "cat4", "cat3")
+    print(a)
+    a.add_node(5, "cat5", "cat4")
+    print(a)
+    a.remove_node("cat5")
+    print(a)
+    a.add_node(6,"cat6", "cat2")
+    a.add_node(7,"cat7", "cat4")
+    print(a)
+    a.remove_node("cat2")
+    a.remove_node("cat3")
+    print(a)
+    a.add_node(2, "cat2", "cat3")
+    a.remove_node("cat3")
+    print(a.find_node("cat2"))
+    print(a)
---
+++ main.py
@@ -0,0 +1,72 @@
+import Database
+import Tree
+
+def startUp():
+	mydb = Database.connector("localhost", "root", "admin", "mydatabase")
+	tree = Tree.Tree(0, "root")
+	tables = Database.getTables(mydb)
+
+	tables.remove(("root", ))
+	tables.remove(("control", ))
+
+	for record in Database.getRecords(mydb, "root"):
+		tree.add_node(record[0], record[1])
+
+	while len(tables) > 0:
+		for table in tables:
+			if tree.find_node(table[0]) != None:
+				for record in Database.getRecords(mydb, table[0]):
+					tree.add_node(record[0], record[1], table[0])
+				tables.remove(table)
+	return (tree,mydb)
+
+def add(data):
+	record = input('Record to add: ')
+	table = input('Table name:')
+	father = data[0].find_node(table)
+	if (father != None):
+		Database.addTable(data[1], table)
+		if(Database.addRecord(data[1], table, record)):
+			record = Database.getRecord(data[1], table, record)[0]
+			data[0].add_node(record[0], record[1], table)
+			return True
+	return False
+
+def delete(data):
+	record = input('Record to delete: ')
+	if (Database.deleteCascade(data[1],record)):
+		data[0].remove_node(record)
+		return True
+	return False
+
+def show(data, nome = ''):
+	data[0].printTable(nome)
+	#TODO
+	return False
+
+if __name__ == "__main__":
+	exit = False
+	data = startUp()	
+
+	while(not(exit)):
+		print("""Select a number:
+1. Add record
+2. Delete record
+3. Show records
+0. Exit
+			""")
+		try:
+			choice = int(input('Selection: '))
+		except:
+			print("That's not a valid option!")
+		if choice == 1:
+			if(add(data)):
+				print("success")
+		elif choice == 2:
+			if(delete(data)):
+				print("success")
+		elif choice == 3:
+			choice = input('Table to print (dont write to print all): ')
+			show(data, choice)
+		elif choice == 0:
+			exit = True
